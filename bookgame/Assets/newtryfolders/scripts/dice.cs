613f47249a546d18c32e5aab86077ce2e1a40993
jayapraneshj <jayapraneshr@gmail.com>
2024-04-14 16:53:16 +0530

    newscene

--- bookgame/Assets/newtryfolders/scripts/dice.cs
+++ bookgame/Assets/newtryfolders/scripts/dice.cs
@@ -1,30 +1,59 @@
-using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections;
 using UnityEngine;
-public class DiceRoll : MonoBehaviour
-{
-    // Array to hold the sides of the dice
-    private int[] sides = { 2, 4, 6, 8, 0 };
 
-    // Function to roll the dice and return the result
-    private int RollDice()
+public class Dice : MonoBehaviour {
+
+    // Array of dice sides sprites to load from Resources folder
+    private Sprite[] diceSides;
+
+    // Reference to sprite renderer to change sprites
+    private SpriteRenderer rend;
+
+	// Use this for initialization
+	private void Start () {
+
+        // Assign Renderer component
+        rend = GetComponent<SpriteRenderer>();
+
+        // Load dice sides sprites to array from DiceSides subfolder of Resources folder
+        diceSides = Resources.LoadAll<Sprite>("DiceSides/");
+	}
+	
+    // If you left click over the dice then RollTheDice coroutine is started
+    private void OnMouseDown()
     {
-        // Get a random index within the range of the sides array
-        int randomIndex = Random.Range(0, sides.Length);
-
-        // Return the value at the randomly selected index
-        return sides[randomIndex];
+        StartCoroutine("RollTheDice");
     }
 
-    // Detect collision with another GameObject
-    private void OnTriggerEnter2D(Collider2D other)
+    // Coroutine that rolls the dice
+    private IEnumerator RollTheDice()
     {
-        // Check if the collision is with a GameObject that has a tag "RollTrigger"
-        if (other.CompareTag("RollTrigger"))
+        // Variable to contain random dice side number.
+        // It needs to be assigned. Let it be 0 initially
+        int randomDiceSide = 0;
+
+        // Final side or value that dice reads in the end of coroutine
+        int finalSide = 0;
+
+        // Loop to switch dice sides ramdomly
+        // before final side appears. 20 itterations here.
+        for (int i = 0; i <= 20; i++)
         {
-            // Roll the dice and print the result
-            int result = RollDice();
-            Debug.Log("You rolled a " + result);
+            // Pick up random value from 0 to 5 (All inclusive)
+            randomDiceSide = Random.Range(0, 4);
+
+            // Set sprite to upper face of dice from array according to random value
+            rend.sprite = diceSides[randomDiceSide];
+
+            // Pause before next itteration
+            yield return new WaitForSeconds(0.05f);
         }
+
+        // Assigning final side so you can use this value later in your game
+        // for player movement for example
+        finalSide = randomDiceSide + 1;
+
+        // Show final dice value in Console
+        Debug.Log(finalSide);
     }
 }
